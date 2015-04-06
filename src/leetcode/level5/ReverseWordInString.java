e257fe7abea37a52f904aad1f4d0acc8a355b520
longwei <longwei.su@gmail.com>
2015-04-05 21:55:42 -0400

    reverse string

--- src/leetcode/level5/ReverseWordInString.java
+++ src/leetcode/level5/ReverseWordInString.java
@@ -23,7 +23,7 @@ public class ReverseWordInString {
             words[j] = tmp;
             i++;j--;
         }
-        return String.join(" ",words);
+        return String.join(" ", words);
     }
 
     //i j alway point to [ ) of a word, same as substring
@@ -41,16 +41,44 @@ public class ReverseWordInString {
         return reversed.toString();
     }
 
-//    If the input string does not contain leading or trailing spaces and the words are
-//    separated by a single space, could you do it in-place without allocating extra space?
-    //sol: reverse whole string to reverse the space, then reverse the word to make it back
 
+
+    //If the input string does not contain leading or trailing spaces and the words are
+    //separated by a single space, could you do it in-place without allocating extra space?
+    //sol: reverse whole string to reverse the space, then reverse the word to make it back
+    public void reverseWordSimple(char[] s) {
+        reverse(s, 0, s.length);
+        for (int i = 0, j = 0; j <= s.length; j++) {
+            if (j == s.length || s[j] == ' ') {
+                reverse(s, i, j);
+                i = j + 1;
+            } }
+    }
+
+    private void reverse(char[] s, int begin, int end){
+        char tmp;
+        end = end - 1;//[) ignore the lasting one
+        while(begin < end){
+            tmp = s[begin];
+            s[begin] = s[end];
+            s[end] = tmp;
+            begin++;end--;
+        }
+    }
+
+//    Rotate an array to the right by k steps in-place without allocating extra space.
+//    For instance, with k = 3, the array [0, 1, 2, 3, 4, 5, 6] is rotated to [4, 5, 6, 0, 1, 2, 3].
+    //reverse 0 n-1
+    //reverse 0 k-1
+    //reverse k n-1
 
 
     public static void main(String[] args) {
         ReverseWordInString test = new ReverseWordInString();
 //        System.out.println("test.reverseWords(the sky is blue) = " + test.reverseWords("the sky is blue"));
-        String output = test.reverserWords("   a   b ");
-        System.out.println("output = " + output);
+        String txt = "the sky is blue";
+        char[] input = txt.toCharArray();
+        test.reverseWordSimple(input);
+        System.out.println(new String(input));
     }
 }
